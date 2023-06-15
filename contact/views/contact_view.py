cb762e767fcb6e819667f51da553a148b12d3939
staviasz <erickesc21@hotmail.com>
2023-06-15 15:52:37 -0300

    desenvolvimento da paginação

--- contact/views/contact_view.py
+++ contact/views/contact_view.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 from contact.models import Contact
 
@@ -7,8 +8,13 @@ def index(request):
   contacts = Contact.objects\
     .filter(show=True)\
     .order_by('-id')
+
+  paginator = Paginator(contacts, 10)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+
   context = {
-    'contacts': contacts,
+    'page_obj': page_obj,
     'site_title': 'Contatos - '
   }
   return render(request, 'contact/index.html', context)
@@ -40,8 +46,13 @@ def search(request):
     )\
     .order_by('-id')
 
+  paginator = Paginator(contacts, 10)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+
+
   context = {
-    'contacts': contacts,
+    'page_obj': page_obj,
     'site_title': 'Search - ',
     'search_value': search_value
   }
