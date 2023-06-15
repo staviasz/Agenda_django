909edb46d8cfeac61d565a573d6df8e201c4bce9
staviasz <erickesc21@hotmail.com>
2023-06-15 11:58:31 -0300

    busca de contatos na base de dados, criação do template de exibição dos dados

--- contact/admin.py
+++ contact/admin.py
@@ -7,11 +7,12 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-  list_display = ('first_name', 'last_name', 'phone')
+  list_display = ('id','first_name', 'last_name', 'phone', 'show',)
   ordering = '-id',
   list_filter = 'created_at',
   list_per_page = 10
   list_max_show_all = 200
+  list_editable = ('first_name', 'last_name', 'show',)
 
 
 @admin.register(models.Category)
