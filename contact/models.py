9e1da87a14b2dcd51c86e852627cb5b086adb257
staviasz <erickesc21@hotmail.com>
2023-06-15 10:35:44 -0300

    configurações nos models, settings, criação do script para a geração de dados

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
+  class Meta:
+    verbose_name = "Category"
+    verbose_name_plural = "Categories" 
+
   name = models.CharField(max_length=50)
 
   def __str__(self) -> str:
@@ -19,6 +24,8 @@ class Contact(models.Model):
   show = models.BooleanField(default=True)
   picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
   category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+  owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
   def __str__(self) -> str:
     return f'{self.first_name} {self.last_name}'
