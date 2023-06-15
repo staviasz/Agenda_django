3340f4fffafc5e80b5c1b7d42fdc181fb289d7e9
staviasz <erickesc21@hotmail.com>
2023-06-15 15:08:42 -0300

    desenvolvimento do header e dosfiltros de busca

--- contact/views/contact_view.py
+++ contact/views/contact_view.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+
 from contact.models import Contact
 
 def index(request):
@@ -6,7 +8,8 @@ def index(request):
     .filter(show=True)\
     .order_by('-id')
   context = {
-    'contacts': contacts
+    'contacts': contacts,
+    'site_title': 'Contatos - '
   }
   return render(request, 'contact/index.html', context)
 
@@ -14,7 +17,32 @@ def index(request):
 def contact(request, contact_id):
   single_contact = get_object_or_404(Contact.objects, pk=contact_id, show=True)
 
+  contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+
   context = {
-    'contact': single_contact
+    'contact': single_contact,
+    'site_title': contact_name
   }
   return render(request, 'contact/contact.html', context)
+
+
+def search(request):
+  search_value = request.GET.get('q', '').strip()
+
+  if search_value == '':
+    return redirect('contact:index')
+
+  contacts = Contact.objects\
+    .filter(show=True)\
+    .filter(
+      Q(first_name__icontains=search_value) |
+      Q(last_name__icontains=search_value)
+    )\
+    .order_by('-id')
+
+  context = {
+    'contacts': contacts,
+    'site_title': 'Search - ',
+    'search_value': search_value
+  }
+  return render(request, 'contact/index.html', context)
