ab125f49757b70bdd0a798dee7873cb47961625d
staviasz <erickesc21@hotmail.com>
2023-06-15 12:40:17 -0300

    desenvolvimento da exibição de unico contato

--- contact/views/contact_view.py
+++ contact/views/contact_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 def index(request):
@@ -9,3 +9,12 @@ def index(request):
     'contacts': contacts
   }
   return render(request, 'contact/index.html', context)
+
+
+def contact(request, contact_id):
+  single_contact = get_object_or_404(Contact.objects, pk=contact_id, show=True)
+
+  context = {
+    'contact': single_contact
+  }
+  return render(request, 'contact/contact.html', context)
