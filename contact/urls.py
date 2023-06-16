c638a21b700f94b413f6f32a75f3017b4aabb97c
staviasz <erickesc21@hotmail.com>
2023-06-15 22:38:49 -0300

    desenvolvimento da opção de deletar contato

--- contact/urls.py
+++ contact/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
   path('', views.index, name='index'),
 
   path('contact/<int:contact_id>/update', views.update, name='update'),
+  path('contact/<int:contact_id>/delete', views.delete, name='delete'),
   path('contact/<int:contact_id>/', views.contact, name='contact'),
   path('contact/create/', views.create, name='create'),
 ]
--- contact/views/contact_form_view.py
+++ contact/views/contact_form_view.py
@@ -51,3 +51,23 @@ def update(request, contact_id):
       return redirect('contact:update', contact_id=contact.pk)
 
   return render(request, 'contact/create.html', context)
+
+def delete(request, contact_id):
+  contact = get_object_or_404(
+    Contact,
+    pk=contact_id,
+    show=True
+  )
+
+  confirmation = request.POST.get('confirmation', 'no')
+
+  if confirmation == 'yes':
+    contact.delete()
+    return redirect('contact:index')
+
+  return render(request,
+                'contact/contact.html',
+                {
+                  'contact': contact,
+                  'confirmation': confirmation
+                })
