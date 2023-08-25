5dad141cc199e1b38ee9540ca5f5588dcf7ca9d5
staviasz <erickesc21@hotmail.com>
2023-08-24 22:26:35 -0300

    corrigindo requirements

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j+*qu!p0tb%1ltr$1fr#8kn-1)*3+3sl=w$pr!k))#*5hoek*g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
